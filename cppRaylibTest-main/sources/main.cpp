2a07976512372252d0baed5ac6eaee59d99b85e4
gustav juho <gustavjuho@gmail.com>
2025-12-02 17:20:41 +0100

    A start menu v3

--- cppRaylibTest-main/sources/main.cpp
+++ cppRaylibTest-main/sources/main.cpp
@@ -21,7 +21,7 @@ using namespace std;
 const int screenWidth = GetMonitorWidth(0);
 const int screenHeight = GetMonitorHeight(0);
 
-int currentMap = 1;
+int currentMap = 0;
 int CurrentTime;
 
 void gameLocic(Cords* cords, Martin* martin, Level* level){
@@ -43,7 +43,7 @@ void gameLocic(Cords* cords, Martin* martin, Level* level){
     cords->ResetMap();
     level->Generate(currentMap, cords);
   }
-  if(IsKeyPressed(KEY_R)){
+  if(IsKeyPressed(KEY_SPACE)){
     level->Generate(currentMap, cords);
   }
   #pragma endregion
@@ -61,9 +61,6 @@ int main()
 
   martin->cords = cords;
 
-  //Gör cordinat fält
-  level->Generate(currentMap, cords);
-
   SetTargetFPS(60);
 
   //sounds
@@ -76,14 +73,32 @@ int main()
     BeginDrawing();
     ClearBackground(BLACK);
 
-    if(currentMap >= 3){
+    //Jag vet att jag borde lägga detta i en funktion fast orkar inte :c    
+    if(currentMap == 0){
+      float Size = 50;
+      DrawText("Use the arrows key to move or use wasd.", 50, 50, Size, SIXSEVEN);
+      DrawText("Try to be as fast as possible to not lose score!", 50, 50 + Size, Size, SIXSEVEN);
+      DrawText("The more you move the more score you will lose!", 50, 50 + (Size * 2), Size, SIXSEVEN);
+      DrawText("If you get stuck press spacebar to reload!", 50, 50 + (Size * 3), Size, SIXSEVEN);
+      DrawText("Push all of the boxes (oranges thing) to the", 50, 100 + (Size * 4), Size, SIXSEVEN);
+      DrawText("win points (the red things) to win!!!", 50, 100 + (Size * 5), Size, SIXSEVEN);
+      DrawText("Pres any key to start!", 50, 100 + (Size * 7), Size, SIXSEVEN);
+      if(GetKeyPressed()){
+        currentMap++;
+        martin->amountOfSteps = 0;
+        CurrentTime = 0;
+        level->Generate(currentMap, cords);
+
+      }
+    }
+    else if(currentMap >= 3){
       float Size = 100;
       int score = ((999999 / martin->amountOfSteps) / (CurrentTime / 60)) * 400;
       DrawText(TextFormat("Amount of steps: %03i", martin->amountOfSteps), 50, 50, Size, SIXSEVEN);
       DrawText(TextFormat("Time: %04i", (CurrentTime / 60)), 50, 50 + Size, Size, SIXSEVEN);
       DrawText(TextFormat("Score: %03i", score), 50, 50 + (Size * 2), Size, SIXSEVEN);
     }
-    else{
+    else if(currentMap >= 3 || currentMap != 0){
       gameLocic(cords, martin, level);
     }
 
